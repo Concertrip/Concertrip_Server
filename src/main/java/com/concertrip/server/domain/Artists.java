343e9d41762be9da9fa91f210dc4cb993bf054b8
hyunjkluz <hyunjk.luz@gmail.com>
2018-12-27 18:02:52 +0900

    update not null attribute

--- src/main/java/com/concertrip/server/domain/Artists.java
+++ src/main/java/com/concertrip/server/domain/Artists.java
@@ -3,6 +3,7 @@ package com.concertrip.server.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -19,8 +20,10 @@ import java.io.Serializable;
 public class Artists implements Serializable {
     @Id
     private String _id;
+    @NonNull
     private String profileImg;
     private String backImg;
+    @NonNull
     private String name;
     private String youtubeUrl;
     private String[] tag;
