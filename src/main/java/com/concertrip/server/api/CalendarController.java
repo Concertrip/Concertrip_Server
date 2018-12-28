61be8017a62a3cb9d750d9628ccd1bc5289c064e
hyunjkluz <hyunjk.luz@gmail.com>
2018-12-28 16:09:05 +0900

    update calendar api

--- src/main/java/com/concertrip/server/api/CalendarController.java
+++ src/main/java/com/concertrip/server/api/CalendarController.java
@@ -7,10 +7,7 @@ import com.concertrip.server.utils.StatusCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created hyunjk on 2018-12-27.
@@ -26,13 +23,42 @@ public class CalendarController {
         this.calendarService = calendarService;
     }
 
+
+    @GetMapping("")
+    public ResponseEntity findAll(@RequestHeader(value = "Autorization") final String token) {
+        try {
+            if (token.isEmpty()) {
+                return new ResponseEntity<>(DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_USER), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(calendarService.getTotalCalendar(token), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(DefaultRes.res(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR), HttpStatus.OK);
+        }
+
+    }
+
     @GetMapping("/events")
     public ResponseEntity findEventsCalendar(@RequestHeader(value = "Autorization") final String token) {
         try {
             if (token.isEmpty()) {
                 return new ResponseEntity<>(DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_USER), HttpStatus.OK);
             }
-            return new ResponseEntity<>(calendarService.findSubscribeEventCalendar(token), HttpStatus.OK);
+            return new ResponseEntity<>(calendarService.getEventsCalendar(token), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(DefaultRes.res(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findArtistCalendar(@PathVariable(value = "id") final String id) {
+        try {
+            if (id.equals("")) {
+                return new ResponseEntity<>(DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_TAG), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(calendarService.getArtistsCalendar(id), HttpStatus.OK);
+            }
         } catch (Exception e) {
             log.error(e.getMessage());
             return new ResponseEntity<>(DefaultRes.res(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR), HttpStatus.OK);
