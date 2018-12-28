5aa40bc0d754ced8bc5ea936547c3aad5d3e3928
hyunjkluz <hyunjk.luz@gmail.com>
2018-12-28 13:35:50 +0900

    modify param name

--- src/main/java/com/concertrip/server/mapper/EventsSubscribeMapper.java
+++ src/main/java/com/concertrip/server/mapper/EventsSubscribeMapper.java
@@ -1,18 +1,21 @@
 package com.concertrip.server.mapper;
 
 import com.concertrip.server.model.EventsSubscribeReq;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface EventsSubscribeMapper {
-    @Insert("INSERT INTO eventSubscribe(eventIdx, userIdx) VALUES (#{eventIdx}, (select userIdx from user where id=#{userId}));")
+    @Insert("INSERT INTO eventSubscribe(eventId, userIdx) VALUES (#{eventId}, (select userIdx from user where id=#{userIdx}));")
     void subscribe(
-            @Param("eventIdx") final String eventIdx,
-            @Param("userId") final String userId
+            @Param("eventId") final String eventId,
+            @Param("userIdx") final String userIdx
     );
+
+    @Delete("DELETE FROM eventSubscribe WHERE userIdx = #{userIdx} AND eventIdx = #{eventId}")
+    void unSubscribe(@Param("userIdx") final String userIdx, @Param("eventId") final String eventId);
+
+    @Select("SELECT * FROM eventSubscribe WHERE userIdx = #{userId} AND eventIdx = #{eventId}")
+    EventsSubscribeReq isSubscribe(@Param("userIdx") final String userIdx, @Param("eventId") final String eventId);
 
     @Select("SELECT * FROM eventSubscribe WHERE userIdx = #{userIdx}")
     EventsSubscribeReq[] findSubscribe(@Param("userIdx") final String userIdx);
