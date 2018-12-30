6d5990aef177a9a8bf602f771bf73a3fd47f8b7f
soonbee <tnswoekd@naver.com>
2018-12-30 14:17:57 +0900

    tmp finish for merge

--- src/main/java/com/concertrip/server/api/GenreController.java
+++ src/main/java/com/concertrip/server/api/GenreController.java
@@ -1,11 +1,14 @@
 package com.concertrip.server.api;
 
 import com.concertrip.server.domain.Genre;
+import com.concertrip.server.model.SubscribeReq;
 import com.concertrip.server.service.GenreService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -27,6 +30,14 @@ public class GenreController {
     @GetMapping("/detail")
     public ResponseEntity findAllById(@RequestParam(value = "id") final String _id) {
         return new ResponseEntity<>(genreService.findById(_id), HttpStatus.OK);
+    }
+
+    @PostMapping("/sub")
+    public ResponseEntity subscribe(
+            @RequestHeader (value = "Authorization") final String token,
+            @RequestBody final SubscribeReq subscribeReq
+    ) {
+        return new ResponseEntity<>(genreService.subscribe(subscribeReq.get_id(), token), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/concertrip/server/service/GenreService.java
+++ src/main/java/com/concertrip/server/service/GenreService.java
@@ -2,6 +2,7 @@ package com.concertrip.server.service;
 
 import com.concertrip.server.dao.GenreRepository;
 import com.concertrip.server.domain.Genre;
+import com.concertrip.server.mapper.GenreSubscribeMapper;
 import com.concertrip.server.model.DefaultRes;
 import com.concertrip.server.utils.ResponseMessage;
 import com.concertrip.server.utils.StatusCode;
@@ -15,9 +16,11 @@ import java.util.List;
 @Service
 public class GenreService {
     private final GenreRepository genreRepository;
+    private final GenreSubscribeMapper genreSubscribeMapper;
 
-    public GenreService(GenreRepository genreRepository) {
+    public GenreService(GenreRepository genreRepository, GenreSubscribeMapper genreSubscribeMapper) {
         this.genreRepository = genreRepository;
+        this.genreSubscribeMapper = genreSubscribeMapper;
     }
 
     public DefaultRes findAll() {
@@ -33,7 +36,7 @@ public class GenreService {
         }
     }
 
-    public DefaultRes save(Genre genre) {
+    public DefaultRes save(final Genre genre) {
         try {
             Genre ret = genreRepository.save(genre);
             return DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_ARTISTS, ret);
@@ -42,7 +45,7 @@ public class GenreService {
         }
     }
 
-    public DefaultRes findById(String _id) {
+    public DefaultRes findById(final String _id) {
         try {
             Genre ret = genreRepository.findBy_idEquals(_id);
             if (ObjectUtils.isEmpty(ret)) {
@@ -50,6 +53,16 @@ public class GenreService {
             }
             return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_ARTISTS, ret);
         } catch (Exception e) {
+            return DefaultRes.res(StatusCode.DB_ERROR, ResponseMessage.DB_ERROR);
+        }
+    }
+
+    public DefaultRes subscribe(final String _id, final String token) {
+        try {
+            genreSubscribeMapper.subscribe(_id, token);
+            return DefaultRes.res(StatusCode.OK, ResponseMessage.SUBSCRIBE_EVENT);
+        } catch (Exception e) {
+            log.error(e.getMessage());
             return DefaultRes.res(StatusCode.DB_ERROR, ResponseMessage.DB_ERROR);
         }
     }
