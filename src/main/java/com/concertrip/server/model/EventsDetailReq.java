903473a72517605834d23fd74251b9b60b95ab8f
hyunjkluz <hyunjk.luz@gmail.com>
2019-01-06 01:51:41 +0900

    change seatPrice data type

--- src/main/java/com/concertrip/server/model/EventsDetailReq.java
+++ src/main/java/com/concertrip/server/model/EventsDetailReq.java
@@ -27,7 +27,7 @@ public class EventsDetailReq {
     private List<CommonListReq> memberList;
     private Date[] date;
     private String[] seatName;
-    private int[] seatPrice;
+    private String[] seatPrice;
     private List<Precaution> precautionList;
     private String eventInfoImg;
 }
