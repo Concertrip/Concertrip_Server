f4d37ccb9d8daf58dae54a1f5da15bd95ec32857
hyunjkluz <hyunjk.luz@gmail.com>
2019-01-09 19:56:05 +0900

    update jwt authorization

--- src/main/java/com/concertrip/server/api/EventsController.java
+++ src/main/java/com/concertrip/server/api/EventsController.java
@@ -3,15 +3,14 @@ package com.concertrip.server.api;
 import com.concertrip.server.domain.Events;
 import com.concertrip.server.model.DefaultRes;
 import com.concertrip.server.service.EventsService;
+import com.concertrip.server.service.JwtService;
 import com.concertrip.server.utils.ResponseMessage;
 import com.concertrip.server.utils.StatusCode;
+import com.concertrip.server.utils.auth.Auth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.xml.ws.Response;
-import java.util.Optional;
 
 /**
  * Created hyunjk on 2018-12-25.
@@ -22,9 +21,11 @@ import java.util.Optional;
 @RequestMapping("api/event")
 public class EventsController {
     private final EventsService eventsService;
+    private final JwtService jwtService;
 
-    public EventsController(EventsService eventsService) {
+    public EventsController(final EventsService eventsService, final JwtService jwtService) {
         this.eventsService = eventsService;
+        this.jwtService = jwtService;
     }
 
 
@@ -33,12 +34,16 @@ public class EventsController {
         return new ResponseEntity<>(eventsService.selectAll(), HttpStatus.OK);
     }
 
+    @Auth
     @GetMapping("detail")
     public ResponseEntity getEventsById(
-            @RequestHeader(value = "Authorization") final Integer token,
+            @RequestHeader(value = "Authorization") final String token,
             @RequestParam(value = "id", defaultValue = "") final String _id) {
-        if (_id.equals("") ) return new ResponseEntity<>(DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_EVENT), HttpStatus.OK);
-        return new ResponseEntity<>(eventsService.findEventsById(token, _id), HttpStatus.OK);
+        if (_id.equals("") ) {
+            return new ResponseEntity<>(DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_EVENT), HttpStatus.OK);
+        }
+        JwtService.Token decodedToken = jwtService.decode(token);
+        return new ResponseEntity<>(eventsService.findEventsById(decodedToken.getUser_idx(), _id), HttpStatus.OK);
     }
 
     @PostMapping("")
